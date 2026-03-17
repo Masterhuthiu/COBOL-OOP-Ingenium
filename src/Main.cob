10166cf19cc800a661956bc64fdaca1cc010b3a3
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-17 11:13:50 +0700

    po

--- src/Main.cob
+++ src/Main.cob
@@ -4,20 +4,27 @@
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        REPOSITORY.
-           CLASS Policy AS "Policy".
+           CLASS Customer AS "Customer"
+           CLASS Order AS "Order"
+           CLASS Product AS "Product".
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 PolObj OBJECT REFERENCE Policy.
+       01 CustObj OBJECT REFERENCE Customer.
+       01 OrderObj OBJECT REFERENCE Order.
+       01 ProdObj OBJECT REFERENCE Product.
 
        PROCEDURE DIVISION.
            DISPLAY "=== MAIN PROGRAM START ==="
 
-           INVOKE Policy "NEW" RETURNING PolObj
-           INVOKE PolObj "Show"
+           INVOKE Customer "NEW" RETURNING CustObj
+           INVOKE CustObj "Show"
 
-           *> Demo InsertPolicy
-           INVOKE Policy "InsertPolicy" USING 800016251, "Nguyen T Thien", "Nguyen T Thien", "PREMIUM PAYING", 2500000
+           INVOKE Order "NEW" RETURNING OrderObj
+           INVOKE OrderObj "Show"
+
+           INVOKE Product "NEW" RETURNING ProdObj
+           INVOKE ProdObj "Show"
 
            DISPLAY "=== MAIN PROGRAM END ==="
            STOP RUN.
