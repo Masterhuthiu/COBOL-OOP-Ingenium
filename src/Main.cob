1c82aa21af1be7c534079ddefc520d053e35c6d4
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-17 12:12:40 +0700

    po

--- src/Main.cob
+++ src/Main.cob
@@ -11,11 +11,12 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 CustObj OBJECT REFERENCE Customer.
-       01 OrderObj OBJECT REFERENCE Order.
-       01 ProdObj OBJECT REFERENCE Product.
-       01 PoObj OBJECT REFERENCE Policy.
-
+       01 CustObj USAGE OBJECT REFERENCE Customer.
+       01 OrderObj USAGE OBJECT REFERENCE Order.
+       01 ProdObj USAGE OBJECT REFERENCE Product.
+       01 PoObj   USAGE OBJECT REFERENCE Policy.
+       
+       
        PROCEDURE DIVISION.
            DISPLAY "=== MAIN PROGRAM START ==="
            INVOKE Policy "NEW" RETURNING PoObj
