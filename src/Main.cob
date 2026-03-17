f7880abda3bce5a3e5a8b63ed434ab418f3b8543
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-17 12:58:25 +0700

    po

--- src/Main.cob
+++ src/Main.cob
@@ -6,17 +6,23 @@
        REPOSITORY.
            CLASS Customer
            CLASS Order
-           CLASS Product.
-      
+           CLASS Product
+           CLASS Policy.
+
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01 CustObj  USAGE OBJECT REFERENCE Customer.
        01 OrderObj USAGE OBJECT REFERENCE Order.
        01 ProdObj  USAGE OBJECT REFERENCE Product.
-      
+       01 PoObj    USAGE OBJECT REFERENCE Policy.
+
+
        PROCEDURE DIVISION.
            DISPLAY "=== MAIN PROGRAM START ==="
+
+           INVOKE Policy "NEW" RETURNING PoObj
+           INVOKE PoObj "Show"
 
            INVOKE Customer "NEW" RETURNING CustObj
            INVOKE CustObj "Show"
