2333e57e97079b83295cefe31d6e89386e23cb04
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-17 13:23:10 +0700

    po

--- src/Main.cob
+++ src/Main.cob
@@ -1,27 +1,17 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      * Copyright (c) 2024 by Your Company. All rights reserved.
+      ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. Main.
-
-       ENVIRONMENT DIVISION.
-       CONFIGURATION SECTION.
-       REPOSITORY.
-           CLASS Customer
-           CLASS Order
-           CLASS Product
-           CLASS Policy.
-      * KHÔNG dùng AS "...", chỉ ghi tên Class. Kết thúc bằng dấu chấm.
-
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
        DATA DIVISION.
+       FILE SECTION.
        WORKING-STORAGE SECTION.
-       01 CustObj  USAGE OBJECT REFERENCE Customer.
-       01 OrderObj USAGE OBJECT REFERENCE Order.
-       01 ProdObj  USAGE OBJECT REFERENCE Product.
-       01 PoObj    USAGE OBJECT REFERENCE Policy.
-      * PHẢI có USAGE và dấu chấm ở cuối mỗi dòng khai báo.
-
        PROCEDURE DIVISION.
-           DISPLAY "=== START ==="
-           INVOKE Customer "NEW" RETURNING CustObj
-           DISPLAY "Success: Objects initialized."
-           STOP RUN.
-
-       END PROGRAM Main.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
