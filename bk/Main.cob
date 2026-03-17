e8175c9e0ad7939b42a7839f016428f0f1375997
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-17 12:22:26 +0700

    po

--- bk/Main.cob
+++ bk/Main.cob
@@ -4,18 +4,23 @@
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        REPOSITORY.
-           CLASS Customer AS "Customer"
-           CLASS Order AS "Order"
-           CLASS Product AS "Product".
+           CLASS Customer 
+           CLASS Order 
+           CLASS Product 
+           CLASS Policy.
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 CustObj OBJECT REFERENCE Customer.
-       01 OrderObj OBJECT REFERENCE Order.
-       01 ProdObj OBJECT REFERENCE Product.
-
+       01 CustObj USAGE OBJECT REFERENCE Customer.
+       01 OrderObj USAGE OBJECT REFERENCE Order.
+       01 ProdObj USAGE OBJECT REFERENCE Product.
+       01 PoObj   USAGE OBJECT REFERENCE Policy.
+       
+       
        PROCEDURE DIVISION.
            DISPLAY "=== MAIN PROGRAM START ==="
+           INVOKE Policy "NEW" RETURNING PoObj
+           INVOKE PoObj "Show"
 
            INVOKE Customer "NEW" RETURNING CustObj
            INVOKE CustObj "Show"
