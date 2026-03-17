854e95aa7de959c2d4c5999fc4a38efbc6a12faf
Dang Nguyen <masterhuthiu@gmail.com>
2026-03-17 11:04:12 +0700

    po

--- src/Main.cob
+++ src/Main.cob
@@ -2,14 +2,12 @@
 
        method-id. Main static.
            invoke type Policy "InsertPolicy" using 
-               800016251, "2014-10-28", 1000027885, "Nguyen T Thien", "1985-09-07",
+               800016251, "2014-10-28", 1000027885, "Nguyen T Thien", "1992-09-07",
                1000027885, "Nguyen T Thien", "1992-09-07",
-               "PREMIUM PAYING", "Life Foresight", "Direct Bill",
+               "PREMIUM PAYING", "PVI Sun Life Foresight", "Direct Bill",
                "2014-08-13", "2014-11-13", null,
                2500000, 2, "QUARTERLY", 0, 2500000, 2500000,
                "VND", 0, 0
-
-           invoke type Policy "SearchPolicy" using 800016251
            goback.
        end method Main.
 
--- src/Policy.cob
+++ src/Policy.cob
@@ -3,68 +3,68 @@
        factory.
        procedure division.
 
-       *> Insert Policy
        method-id. InsertPolicy static.
-           procedure division using by value policyId as binary-long
-                                      by value effectiveDate as string
-                                      by value ownerId as binary-long
-                                      by value ownerName as string
-                                      by value ownerBirth as string
-                                      by value insuredId as binary-long
-                                      by value insuredName as string
-                                      by value insuredBirth as string
-                                      by value status as string
-                                      by value product as string
-                                      by value billing as string
-                                      by value issueDate as string
-                                      by value paidToDate as string
-                                      by value overloanExpiry as string
-                                      by value amountBilled as binary-long
-                                      by value suffix as binary-long
-                                      by value premiumMode as string
-                                      by value sundryAmount as binary-long
-                                      by value truePremium as binary-long
-                                      by value lastModePremium as binary-long
-                                      by value currency as string
-                                      by value overloanLeast as binary-long
-                                      by value fullApaLeast as binary-long.
+           procedure division using by value pid as binary-long
+                                      by value eff as string
+                                      by value oid as binary-long
+                                      by value oname as string
+                                      by value obirth as string
+                                      by value iid as binary-long
+                                      by value iname as string
+                                      by value ibirth as string
+                                      by value st as string
+                                      by value prod as string
+                                      by value bill as string
+                                      by value issue as string
+                                      by value paid as string
+                                      by value overloan as string
+                                      by value amt as binary-long
+                                      by value suf as binary-long
+                                      by value pmode as string
+                                      by value sundry as binary-long
+                                      by value true as binary-long
+                                      by value last as binary-long
+                                      by value curr as string
+                                      by value oleast as binary-long
+                                      by value apa as binary-long.
+
            declare conn type NpgsqlConnection
                = new NpgsqlConnection("Host=localhost;Username=postgres;Password=postgres;Database=insurance").
            invoke conn "Open".
 
-           declare cmd type NpgsqlCommand
-               = new NpgsqlCommand(
-                 "INSERT INTO policy (policy_id,effective_date,owner_id,owner_name,owner_birth_date,insured_id,insured_name,insured_birth_date,current_status,product_name,billing_type,issue_date,paid_to_date,overloan_expiry_date,amount_billed,suffix,premium_mode,sundry_amount,true_premium,last_mode_premium,currency,overloan_least_amount,full_apa_least_amount) 
-                  VALUES (@pid,@eff,@oid,@oname,@obirth,@iid,@iname,@ibirth,@st,@prod,@bill,@issue,@paid,@overloan,@amt,@suf,@pmode,@sundry,@true,@last,@curr,@oleast,@apa)", conn).
+           declare sqlIns as string =
+             "INSERT INTO policy (policy_id,eff_date,owner_id,owner_name,owner_birth,insured_id,insured_name,insured_birth,status,product,billing,issue_date,paid_to,overloan_exp,amt_billed,suffix,prem_mode,sundry_amt,true_prem,last_prem,curr,overloan_least,apa_least) " &
+             "VALUES (@pid,@eff,@oid,@oname,@obirth,@iid,@iname,@ibirth,@st,@prod,@bill,@issue,@paid,@overloan,@amt,@suf,@pmode,@sundry,@true,@last,@curr,@oleast,@apa)".
 
-           *> mapping parameters
-           invoke cmd "Parameters.AddWithValue" using "pid", policyId.
-           invoke cmd "Parameters.AddWithValue" using "eff", effectiveDate.
-           invoke cmd "Parameters.AddWithValue" using "oid", ownerId.
-           invoke cmd "Parameters.AddWithValue" using "oname", ownerName.
-           invoke cmd "Parameters.AddWithValue" using "obirth", ownerBirth.
-           invoke cmd "Parameters.AddWithValue" using "iid", insuredId.
-           invoke cmd "Parameters.AddWithValue" using "iname", insuredName.
-           invoke cmd "Parameters.AddWithValue" using "ibirth", insuredBirth.
-           invoke cmd "Parameters.AddWithValue" using "st", status.
-           invoke cmd "Parameters.AddWithValue" using "prod", product.
-           invoke cmd "Parameters.AddWithValue" using "bill", billing.
-           invoke cmd "Parameters.AddWithValue" using "issue", issueDate.
-           invoke cmd "Parameters.AddWithValue" using "paid", paidToDate.
-           invoke cmd "Parameters.AddWithValue" using "overloan", overloanExpiry.
-           invoke cmd "Parameters.AddWithValue" using "amt", amountBilled.
-           invoke cmd "Parameters.AddWithValue" using "suf", suffix.
-           invoke cmd "Parameters.AddWithValue" using "pmode", premiumMode.
-           invoke cmd "Parameters.AddWithValue" using "sundry", sundryAmount.
-           invoke cmd "Parameters.AddWithValue" using "true", truePremium.
-           invoke cmd "Parameters.AddWithValue" using "last", lastModePremium.
-           invoke cmd "Parameters.AddWithValue" using "curr", currency.
-           invoke cmd "Parameters.AddWithValue" using "oleast", overloanLeast.
-           invoke cmd "Parameters.AddWithValue" using "apa", fullApaLeast.
+           declare cmd type NpgsqlCommand = new NpgsqlCommand(sqlIns, conn).
+
+           invoke cmd "Parameters.AddWithValue" using "pid", pid.
+           invoke cmd "Parameters.AddWithValue" using "eff", eff.
+           invoke cmd "Parameters.AddWithValue" using "oid", oid.
+           invoke cmd "Parameters.AddWithValue" using "oname", oname.
+           invoke cmd "Parameters.AddWithValue" using "obirth", obirth.
+           invoke cmd "Parameters.AddWithValue" using "iid", iid.
+           invoke cmd "Parameters.AddWithValue" using "iname", iname.
+           invoke cmd "Parameters.AddWithValue" using "ibirth", ibirth.
+           invoke cmd "Parameters.AddWithValue" using "st", st.
+           invoke cmd "Parameters.AddWithValue" using "prod", prod.
+           invoke cmd "Parameters.AddWithValue" using "bill", bill.
+           invoke cmd "Parameters.AddWithValue" using "issue", issue.
+           invoke cmd "Parameters.AddWithValue" using "paid", paid.
+           invoke cmd "Parameters.AddWithValue" using "overloan", overloan.
+           invoke cmd "Parameters.AddWithValue" using "amt", amt.
+           invoke cmd "Parameters.AddWithValue" using "suf", suf.
+           invoke cmd "Parameters.AddWithValue" using "pmode", pmode.
+           invoke cmd "Parameters.AddWithValue" using "sundry", sundry.
+           invoke cmd "Parameters.AddWithValue" using "true", true.
+           invoke cmd "Parameters.AddWithValue" using "last", last.
+           invoke cmd "Parameters.AddWithValue" using "curr", curr.
+           invoke cmd "Parameters.AddWithValue" using "oleast", oleast.
+           invoke cmd "Parameters.AddWithValue" using "apa", apa.
+
            invoke cmd "ExecuteNonQuery".
        end method InsertPolicy.
 
-       *> Update, Delete, Search tương tự (UpdatePolicy, DeletePolicy, SearchPolicy)
        end factory.
 
        end class Policy.
